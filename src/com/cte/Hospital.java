b47d593323ebd288d24bf0403a983e76092b8bdb
tallner <ctallner@hotmail.com>
2022-03-02 00:19:18 +0100

    Hospital class added
    This class is used to collect employees, patients and rooms

--- src/com/cte/Hospital.java
+++ src/com/cte/Hospital.java
@@ -1,4 +1,41 @@
 package com.cte;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hospital {
+    private ArrayList<Patient> patients = new ArrayList<>();
+    private ArrayList<Employee> employees = new ArrayList<>();
+
+    public Hospital() {
+    }
+
+    //adds a patient to the list
+    public void addPatient(Patient patient){
+        patients.add(patient);
+    }
+
+    //get list of patients
+    public List<Patient> getPatientList() {
+        return patients;
+    }
+
+    //adds an employee to the list
+    public void addEmployee(Employee employee){
+        employees.add(employee);
+    }
+
+    //get list of employees
+    public List<Employee> getEmployeeList() {
+        return employees;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Hospital{" +
+                ", patients=" + patients +
+                 ", employees=" + employees +
+                '}';
+    }
 }
